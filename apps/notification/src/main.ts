71c412782bd72ab769ece1ee2e5c2ec630df3396
Alison Simiao <alison.simiao@gmail.com>
2025-04-15 21:56:29 -0300

    delete

--- apps/notification/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { NotificationModule } from './notification.module';
-
-async function bootstrap() {
-  console.log(process.env);
-  const app = await NestFactory.create(NotificationModule);
-  await app.listen(process.env.port ?? 3001);
-}
-bootstrap();
