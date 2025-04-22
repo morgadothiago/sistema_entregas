bbe2d9f4c0bc6bfeaf0af2b74f83cb1ff83a1597
thiago morgado <thiago_morgado@hotmail.com>
2025-04-22 15:21:57 -0300

    feat(SIgnUp) create page signup

---
+++ web/src/app/components/TextInput/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  labelName: string;
+  className: string;
+  placeholder?: string;
+  required?: boolean; // Corrected 'require' to 'required'
+  classNameInput?: string;
+}
+
+export function TextInput({
+  labelName, // Adicionei esta linha
+  className,
+
+  ...rest
+}: TextInputProps) {
+  return (
+    <div className={className}>
+      <Label className="mb-3 text-[#003B73] text-base sm:text-lg md:text-xl font-semibold">
+        {labelName}
+      </Label>
+      <Input
+        className={`w-full border border-blue-500 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-200 ease-in-out ${rest.classNameInput}`}
+        {...rest}
+      />
+    </div>
+  );
+}
