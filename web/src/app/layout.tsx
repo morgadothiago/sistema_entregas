f87e3bc88f6ac8dfab275383dd8a639764b26283
Alison Simiao <alison.simiao@gmail.com>
2025-04-27 01:05:48 -0300

    refact: centraliza providers

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { AuthProvider } from "@/app/context";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
+        <Providers>
         <body
           suppressHydrationWarning
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -34,7 +34,7 @@ export default function RootLayout({
           {children}
           <Toaster />
         </body>
-      </AuthProvider>
+        </Providers>
     </html>
   );
 }
