60b4da16bfd4840497e3488443ec6fc2e2b86bcd
thiago morgado <thiago_morgado@hotmail.com>
2025-05-16 23:38:01 -0300

    feat: Create SplashScreen Anitemeted or progressbar

--- entregador/App.tsx
+++ entregador/App.tsx
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import 
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_600SemiBold,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "./src/components/Loading";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_600SemiBold,
+    Roboto_700Bold,
+  });
+  const [appReady, setAppReady] = useState(false);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  // Mostra a splash personalizada até chamar onFinish no Loading
+  if (!appReady) {
+    return <Loading onFinish={() => setAppReady(true)} />;
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar barStyle={"light-content"} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
