d53cf85cc777ce6fcc15ee54241af3ccaefc27c9
thiago morgado <thiago_morgado@hotmail.com>
2025-05-18 21:45:49 -0300

    feat: implement SignIn screen with custom input components and theme support

--- entregador/src/screen/SignIn/index.tsx
+++ entregador/src/screen/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "react-native";
+import { Button, Keyboard, TouchableWithoutFeedback } from "react-native";
 
 import { useAuth } from "../../context/AuthContext";
 import { useNavigation } from "@react-navigation/native";
@@ -6,10 +6,22 @@ import { useNavigation } from "@react-navigation/native";
 import type { RootStackParamList } from "../../types/RootParamsList";
 import type { StackNavigationProp } from "@react-navigation/stack";
 
-import { Input } from "../../components/Input";
-import { GradientBackground } from "./styles";
+import { TextInput } from "../../components/Input";
+import {
+  GradientBackground,
+  ImageContainer,
+  Image,
+  Title,
+  FormArea,
+  Footer,
+} from "./styles";
+
+import Logo from "../../../assets/ios-light.png";
+import { KeyboardAvoidingView } from "react-native";
+import { useForm } from "react-hook-form";
 
 export default function SignInScreen() {
+  const { control } = useForm();
   const { login, isAuthenticated } = useAuth();
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
   // Simulate a login function
@@ -29,9 +41,47 @@ export default function SignInScreen() {
   };
 
   return (
-    <GradientBackground>
-      <Button title="Entrar" onPress={handleLogin} />
-      <Input />
-    </GradientBackground>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <GradientBackground>
+        <>
+          <ImageContainer>
+            <Image source={Logo} resizeMode="cover" />
+            <Title>Sign In</Title>
+          </ImageContainer>
+
+          <KeyboardAvoidingView behavior="padding">
+            <FormArea>
+              <TextInput
+                formProps={{
+                  name: "email",
+                  control,
+                }}
+                inputProps={{
+                  placeholder: "Email",
+                  placeholderTextColor: "#FFF",
+                }}
+                icon="user"
+              />
+
+              <TextInput
+                formProps={{
+                  name: "email",
+                  control,
+                }}
+                inputProps={{
+                  placeholder: "Password",
+                  placeholderTextColor: "#FFF",
+                }}
+                icon="lock"
+              />
+            </FormArea>
+          </KeyboardAvoidingView>
+
+          <Footer>
+            <Button title="Login" onPress={handleLogin} />
+          </Footer>
+        </>
+      </GradientBackground>
+    </TouchableWithoutFeedback>
   );
 }
--- entregador/src/screen/SignIn/styles.ts
+++ entregador/src/screen/SignIn/styles.ts
@@ -1,12 +1,55 @@
 import styled from "styled-components/native";
 import { LinearGradient } from "expo-linear-gradient";
 
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+interface ImageProps {
+  keyboardOpen: boolean;
+}
+
 export const GradientBackground = styled(LinearGradient).attrs({
-  colors: ["#00387A", "#0052B4", "#4A90E2", "#A9CCE3"], // do azul mais escuro ao mais claro/transparente
+  colors: ["#00387A", "#0052B4", "#4A90E2", "#A9CCE3"],
   start: { x: 0.5, y: 0 },
   end: { x: 0.5, y: 1.5 },
 })`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding: ${height * 0.03}px;
+`;
+
+export const ImageContainer = styled.View`
+  width: 100%;
+  height: ${height * 0.2}px;
+  align-items: center;
+`;
+
+export const Image = styled.Image<ImageProps>`
+  width: ${({ keyboardOpen }: ImageProps) =>
+    keyboardOpen ? "0px" : `${width * 0.3}px`};
+  height: ${({ keyboardOpen }: ImageProps) =>
+    keyboardOpen ? `${height * 0.2}px` : `${height * 0.15}px`};
+  border-radius: 10px;
+`;
+
+export const Title = styled.Text`
+  font-size: ${width * 0.06}px;
+  font-weight: bold;
+  color: #fff;
+`;
+
+export const FormArea = styled.View`
+  width: 100%;
+
+  margin-bottom: ${height * 0.03}px;
+`;
+
+export const Footer = styled.View`
+  position: absolute;
+  bottom: ${height * 0.01}px;
+  width: 100%;
+  align-items: center;
+  padding: ${height * 0.025}px;
 `;
