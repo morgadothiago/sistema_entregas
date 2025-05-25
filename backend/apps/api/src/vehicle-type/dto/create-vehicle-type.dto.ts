d5afd0880adb8546804cf26dda4026074548f01f
Alison Simiao <alison.simiao@gmail.com>
2025-05-25 19:20:35 -0300

    feat: adicionar novos campos e lógica de manipulação no DTO de veículo

--- backend/apps/api/src/vehicle-type/dto/create-vehicle-type.dto.ts
+++ backend/apps/api/src/vehicle-type/dto/create-vehicle-type.dto.ts
@@ -1,30 +1,48 @@
 import { ApiProperty } from "@nestjs/swagger";
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  Min,
-} from "class-validator";
+import { IsNotEmpty, IsString, IsNumber } from "class-validator";
 
 export class CreateVehicleTypeDto {
   @ApiProperty({
     description: "nome do tipo de veiculo",
     example: "carro",
   })
-  @IsOptional()
   @IsString()
   @IsNotEmpty()
   type: string;
 
   @ApiProperty({
-    required: false,
-    minimum: 0,
-    description: "valor em reais por km",
-    example: "10.50",
+    description: "tarifa base do veículo",
+    example: 100.0,
+    type: Number,
   })
-  @Min(0)
   @IsNumber()
   @IsNotEmpty()
-  pricePerKm: number;
+  tarifaBase: number;
+
+  @ApiProperty({
+    description: "valor por KM adicional",
+    example: 10.5,
+    type: Number,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  valorKMAdicional: number;
+
+  @ApiProperty({
+    description: "valor por parada adicional",
+    example: 25.0,
+    type: Number,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  ParadaAdicional: number;
+
+  @ApiProperty({
+    description: "valor por ajudante adicional",
+    example: 50.0,
+    type: Number,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  AjudanteAdicional: number;
 }
--- backend/apps/api/src/vehicle-type/dto/update-vehicle-type.dto.ts
+++ backend/apps/api/src/vehicle-type/dto/update-vehicle-type.dto.ts
@@ -1,11 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import {
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-  Min,
-} from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class UpdateVehicleTypeDto {
   @ApiProperty({
@@ -19,14 +13,42 @@ export class UpdateVehicleTypeDto {
   type?: string;
 
   @ApiProperty({
-    required: false,
-    minimum: 0,
-    description: "valor em reais por km",
-    example: "10.50",
+    description: "tarifa base do veículo",
+    example: 100.0,
+    type: Number,
   })
+  @IsNumber({ allowInfinity: false })
   @IsOptional()
-  @Min(0)
-  @IsNumber()
   @IsNotEmpty()
-  pricePerKm?: number;
+  tarifaBase?: number;
+
+  @ApiProperty({
+    description: "valor por KM adicional",
+    example: 10.5,
+    type: Number,
+  })
+  @IsNumber({ allowInfinity: false })
+  @IsOptional()
+  @IsNotEmpty()
+  valorKMAdicional?: number;
+
+  @ApiProperty({
+    description: "valor por parada adicional",
+    example: 25.0,
+    type: Number,
+  })
+  @IsNumber({ allowInfinity: false })
+  @IsOptional()
+  @IsNotEmpty()
+  ParadaAdicional?: number;
+
+  @ApiProperty({
+    description: "valor por ajudante adicional",
+    example: 50.0,
+    type: Number,
+  })
+  @IsNumber({ allowInfinity: false })
+  @IsOptional()
+  @IsNotEmpty()
+  AjudanteAdicional?: number;
 }
