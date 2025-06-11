5c7b6378cdd87ec0b2c014fb3ba075feceae3cc7
Alison Simiao <alison.simiao@gmail.com>
2025-06-11 10:26:33 -0300

    fix(cors): remover método HEAD da configuração de CORS

--- backend/apps/api/src/main.ts
+++ backend/apps/api/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    methods: "GET,PATCH,POST,DELETE",
     preflightContinue: false,
   });
 
