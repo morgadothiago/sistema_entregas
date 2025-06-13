b8e63836b7d770aac7d5a1f63e0ba0a3469c5b9c
thiago morgado <thiago_morgado@hotmail.com>
2025-06-13 16:54:58 -0300

    feat(signup): adicionar tela de cadastro com fundo e cabeçalho

--- entregador/src/screen/Signup/index.tsx
+++ entregador/src/screen/Signup/index.tsx
@@ -1,16 +1,28 @@
 import React from "react";
-
 import {
+  ImageBackground,
   Text,
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
+import { Feather } from "@expo/vector-icons"; // Correção na importação do ícone
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import Header from "../../components/Header";
 
 export default function Signup() {
+  const navigation = useNavigation();
   return (
     <TouchableWithoutFeedback>
-      <Text></Text>
+      <ImageBackground
+        source={require("../../../assets/SplashScreen.png")}
+        style={{ flex: 1 }}
+      >
+        <SafeAreaView>
+          <Header title="Cadastra se" icon="arrow-left" onPress={() => {}} />
+        </SafeAreaView>
+      </ImageBackground>
     </TouchableWithoutFeedback>
   );
 }
