670a2151d6174d02af10672469306d3e977290dd
thiago morgado <thiago_morgado@hotmail.com>
2025-06-25 23:20:39 -0300

    feat: add MapSimulate component for route visualization using Leaflet

---
+++ web/src/app/(private)/dashboard/simulate/MapSimulate.tsx
@@ -0,0 +1,48 @@
+"use client";
+import {
+  MapContainer,
+  TileLayer,
+  Polyline,
+  Marker,
+  Popup,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix para ícones do Leaflet no Next.js
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
+
+interface MapSimulateProps {
+  route: [number, number][];
+}
+
+export default function MapSimulate({ route }: MapSimulateProps) {
+  return (
+    <MapContainer
+      center={route[0]}
+      zoom={14}
+      scrollWheelZoom={false}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Polyline positions={route} color="blue" />
+      <Marker position={route[0]}>
+        <Popup>Origem</Popup>
+      </Marker>
+      <Marker position={route[route.length - 1]}>
+        <Popup>Destino</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
