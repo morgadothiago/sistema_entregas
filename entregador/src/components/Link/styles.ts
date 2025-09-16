6dcd8c7040a950025742e24777d9e70c1664a744
thiago morgado <thiago_morgado@hotmail.com>
2025-09-16 16:00:22 -0300

    refazendo mobile

--- entregador/src/components/Link/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-import { theme } from "../../global/theme";
-
-export const LinkContainer = styled.TouchableOpacity``;
-export const LinkTitle = styled.Text`
-  color: ${theme.colors.button};
-  font-size: 16px;
-  font-weight: bold;
-`;
